2f3852154b39f506ff69e9fd83502d82b8cee416
pleikuyy-eng <pleikuyy@gmail.com>
2026-02-18 17:18:22 +0700

    Update CLIENT_ID in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new Client({
 });
 
 const TOKEN = process.env.TOKEN;
-const CLIENT_ID = "1472623586895462491"; // vào Discord Developer Portal copy
+const CLIENT_ID = "1473553985033207981"; // vào Discord Developer Portal copy
 const GUILD_ID = "1472644187269758978"; // chuột phải server > copy ID
 
 client.once("ready", async () => {
