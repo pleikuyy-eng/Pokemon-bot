5af005307cdeb1907ea3d69a50eaf3ce100b9116
pleikuyy-eng <pleikuyy@gmail.com>
2026-02-18 14:24:06 +0700

    Initialize Discord bot and web server

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
+app.listen(3000, () => {
+  console.log("Web server running");
+});
